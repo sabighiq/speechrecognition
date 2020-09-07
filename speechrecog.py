942e3ffc1d5a143cc4da0f1a1b4d02a6a3437759
sabighiq <70857920+sabighiq@users.noreply.github.com>
2020-09-07 11:36:35 +0530

    Add files via upload

---
+++ speechrecog.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+import webbrowser as wb
+ 
+r1 = sr.Recognizer()
+r2 = sr.Recognizer()
+r3 = sr.Recognizer()
+ 
+with sr.Microphone() as source:
+    print('[search github: search youtube]')
+    print('speak now')
+    audio = r3.listen(source)
+ 
+if 'github' in r2.recognize_google(audio):
+    r2 = sr.Recognizer()
+    url = 'https://www.github.com/'
+    with sr.Microphone() as source:
+        print('search your query')
+        audio = r2.listen(source)
+ 
+        try:
+            get = r2.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print('error')
+        except sr.RequestError as e:
+            print('failed'.format(e))
+ 
+if 'video' in r1.recognize_google(audio):
+    r1 = sr.Recognizer()
+    url = 'https://www.youtube.com/results?search_query='
+    with sr.Microphone() as source:
+        print('search for a video')
+        audio = r2.listen(source)
+ 
+        try:
+            get = r1.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+ 
+        except sr.UnknownValueError:
+            print('could not understand')
+        except sr.RequestError as e:
+            print('failed to get results'.format(e))
+
+
+
